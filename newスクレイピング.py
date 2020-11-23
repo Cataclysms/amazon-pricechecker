d8a2e57fc8529e289f3403b5fca38301dcf921e4
Cataclysms <42298012+Cataclysms@users.noreply.github.com>
2020-11-23 13:39:39 +0900

    Add files via upload

--- newスクレイピング.py
+++ newスクレイピング.py
@@ -5,10 +5,12 @@ import schedule
 import time
 import datetime
 
-REMAINING_SEC = 10#間隔
+REMAINING_SEC = 60#間隔
 line = "https://notify-api.line.me/api/notify"
 access_token = ''#line notify アクセストークン
 headers = {'Authorization': 'Bearer ' + access_token}
+
+nowprice = None
 
 def sendmessage(nmessage):
 	message = nmessage
@@ -16,18 +18,24 @@ def sendmessage(nmessage):
 	r = requests.post(line, headers=headers, params=payload,)
 
 def get_price():
-	source = ""#商品ページURL(新品最安昇順)
+	global nowprice
+	source = "https://www.amazon.co.jp/gp/offer-listing/B07X47QTN3/ref=dp_olp_ALL_mbc?ie=UTF8&condition=ALL"
 	url = requests.get(source)
 	soup = bs4.BeautifulSoup(url.text,"lxml")
-	time = str(datetime.datetime.now())
 	html = soup.select(".a-size-large")
+	time = str(datetime.datetime.now())
+	#print(soup)
 	title = html[0].text.replace(' ','')#str商品title
 	lowestprice = html[1].text.replace(',','').replace(' ','').replace('￥','')#str最低価格
 	print(title+"現在価格 : "+lowestprice+"円")
 	if int(lowestprice) < 23000:#条件
-		print("通知を送信します・・・")
-		sendmessage("\n"+time+title +"\n現在の価格: "+str(lowestprice)+"円")
-		sendmessage(source)
+			if nowprice == lowestprice:
+				print("前回取得した金額と同じため送信されませんでした")
+				return;
+			print("通知を送信します・・・")
+			sendmessage("\n"+time+title +"\n現在の価格: "+str(lowestprice)+"円")
+			sendmessage(source)
+	nowprice = lowestprice
 
 if __name__ == '__main__':
 	print("========Amazon価格監視ツール========")
